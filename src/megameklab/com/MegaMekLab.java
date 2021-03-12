8cbad99ade6ca923f93fcea778f2924fc10bfea4
Carl Spain <neoancient@megamek.org>
2021-03-12 14:49:08 -0600

    Ready to develop <v0.48.1>

--- src/megameklab/com/MegaMekLab.java
+++ src/megameklab/com/MegaMekLab.java
@@ -49,7 +49,7 @@ import megameklab.com.util.CConfig;
 import megameklab.com.util.UnitUtil;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.48.0";
+    public static final String VERSION = "0.48.1-SNAPSHOT";
 
     private static MMLogger logger = null;
 
